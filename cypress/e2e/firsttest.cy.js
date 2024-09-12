bf00d2fc10da744fb1a194f28b2f7cee916a6559
iamhari07 <mandalapuhari45@gmail.com>
2024-09-12 15:42:59 +0530

    updated

---
+++ cypress/e2e/firsttest.cy.js
@@ -0,0 +1,9 @@
+describe('Navigate to the facebook', () => {
+  it('Should perform login', () => {
+  cy.visit('https://www.facebook.com');
+  cy.wait(3000);
+  cy.get("#email").type("demotest@gmail.com");
+  cy.get("#pass").type("demotest");
+  cy.get('[data-testid="royal_login_button"]').click();
+  });
+}); 
